456d405556723cea1e5095fc7fb68aa4d2b2afba
Andrei Micu <amicu@wehkamp.nl>
2019-12-20 15:48:16 +0200

    Course 8

--- src/main/scala/playground/Playground.scala
+++ src/main/scala/playground/Playground.scala
@@ -6,4 +6,17 @@ object Playground extends App {
 
   val actorSystem = ActorSystem("HelloAkka")
   println(actorSystem.name)
+
+  class B[_]
+  class A {
+    def doStuff(implicit b: B[A]): Unit = {
+    }
+  }
+  object A {
+    implicit val myVal: B[A] = new B[A]
+  }
+
+  def takeAImplicitly(a: A): A = a
+
+  takeAImplicitly(new A).doStuff
 }
